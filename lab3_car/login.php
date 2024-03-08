7866f97ea15f483d7f2595668329be46b2ac0bcc
borgarmae <subaldomae29@gmail.com>
2024-03-08 21:34:52 +0800

    Update login.php
    Added minor changes and comments

--- lab3_car/login.php
+++ lab3_car/login.php
@@ -2,9 +2,10 @@
 <html>
 
 <head>
+    <!-- Page title -->
     <title>LOGIN</title>
     <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
@@ -62,69 +63,74 @@
             box-sizing: padding-box;
         }
 
-        .form-control{
+        .form-control {
             margin: 5px;
         }
-
-    
     </style>
 
     <head>
-    <title>SIGN UP</title>
+        <title>SIGN UP</title>
     </head>
+
 <body>
-<form action="index.php" method="post">
+    <form action="index.php" method="post">
 
-<div class="container mt-4" style="max-width: 500px;">
-    <div class="custom-form">
-        <h1 class="text-center">LOGIN</h1>
-        <p class="text-center">Please enter your credentials.</p>
-        <hr>
+        <div class="container mt-4" style="max-width: 500px;">
+            <div class="custom-form">
+                <h1 class="text-center">LOGIN</h1>
+                <p class="text-center">Please enter your credentials.</p>
+                <hr>
 
-        <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?>
-            <p>
-            <?php } ?>
+                <!-- Display error message -->
+                <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?>
+                    <p>
+                    <?php } ?>
 
-            <!-- Display success message -->
-            <?php if (isset($_GET['success'])) { ?>
-            <p class="success"><?php echo $_GET['success']; ?>
-            <p>
-            <?php } ?>
+                    <!-- Display success message -->
+                    <?php if (isset($_GET['success'])) { ?>
+                    <p class="success"><?php echo $_GET['success']; ?>
+                    <p>
+                    <?php } ?>
 
-            <div class="form-group">
-    <div class="input-group">
-        <div class="input-group-prepend">
-            <span class="input-group-text">
-                <i class="fa-solid fa-user" style="color: #f051a6;"></i>
-            </span>
-        </div>
-    <input type="text" name="uname" class="form-control" placeholder="Username">
-</div>
-</div>
+                    <!-- Username input field -->
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa-solid fa-user" style="color: #f051a6;"></i>
+                                </span>
+                            </div>
+                            <input type="text" name="uname" class="form-control" placeholder="Username">
+                        </div>
+                    </div>
+
+                    <!-- Password input field -->
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa-solid fa-lock" style="color: #f051a6;"></i>
+                                </span>
+                            </div>
+                            <input type="password" name="password" class="form-control" placeholder="Enter your password">
+                        </div>
+                    </div><br>
+
+                    <!-- Login button -->
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-pink btn-block">Sign in</button>
+                    </div>
 
 
-<div class="form-group">
-<div class="input-group">
-<div class="input-group-prepend">
-<span class="input-group-text">
-    <i class="fa-solid fa-lock" style="color: #f051a6;"></i>
-</span>
-</div>
-<input type="password" name="password" class="form-control" placeholder="Enter your password">
-</div>
-</div><br>
+                    <!-- Login Link -->
+                    <div class="mt-3 text-center">
+                        <a href="signup.php">Create an account</a>
+                    </div>
 
-            <button type="submit" class="btn btn-pink btn-block">Sign in</button>
-
-            <!-- Login Link -->
-            <div class="mt-3 text-center">
-                <a href="signup.php">Create an account</a>
             </div>
 
-    </div>
-
-</form>
+    </form>
 </body>
 
 </html>
